0a270b1f74d8a1f0aa7cc33e5aa375966327739c
Andrew Drozdov <andrew@mrdrozdov.com>
2017-07-12 12:00:21 -0400

    Pre-runnable state

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'bcrypt',
     'plaster_pastedeploy',
     'pyramid >= 1.9a',
     'pyramid_debugtoolbar',
