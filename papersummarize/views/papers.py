253dab9a5b66d82eef032be119d4417dcfaf9c66
Andrew Drozdov <andrew@mrdrozdov.com>
2017-07-23 11:11:12 -0400

    Change some of the view logic.

--- papersummarize/views/papers.py
+++ papersummarize/views/papers.py
@@ -12,23 +12,43 @@ from ..shared.enums import ENUM_User_is_leader, ENUM_Summary_visibility, ENUM_Su
 @view_config(route_name='view_paper', renderer='../templates/paper.jinja2',
              permission='view')
 def view_paper(request):
+    """
+    Summary {
+        visibility: public | members,
+        review_status: reviewed | under_review,
+    }
+
+    Constraints:
+        - in order to be public, the summary must be reviewed (not all reviewed summaries are public)
+
+    Different Levels of "Visibility"
+
+    - Public: Everyone can see it. { visibility=public, review_status=reviewed }
+    - Members+Reviewed: People that have written a summary (for this paper) can see it. { visibility=members, review_status=under_review }
+    - Members+UnderReview: People that have written a summary (for this paper) can see it, only if their summary has been reviewed. { visibility=members, review_status=under_review }
+
+    """
     paper = request.context.paper
-    summary_written = request.dbsession.query(Summary).filter_by(creator=request.user, paper=paper).count() == 1
+    summary = request.dbsession.query(Summary).filter_by(creator=request.user, paper=paper).first()
+    has_wrote = summary is not None
+    has_been_reviewed = has_wrote and summary.review_status == ENUM_Summary_review_status['reviewed']
     num_summaries = request.dbsession.query(Summary).filter_by(paper=paper).count()
 
     if request.user is not None:
         # TODO: If the user has written a summary, always show it.
-        if request.user.is_leader == ENUM_User_is_leader['True'] or summary_written:
+        if request.user.is_leader == ENUM_User_is_leader['True'] or has_been_reviewed:
             summaries = request.dbsession.query(Summary).filter_by(paper=paper).all()
+        elif has_wrote:
+            summaries = request.dbsession.query(Summary).filter_by(paper=paper, review_status=ENUM_Summary_review_status['reviewed']).all()
         else:
-            summaries = request.dbsession.query(Summary).filter_by(paper=paper, visibility=ENUM_Summary_review_status['reviewed']).all()
+            summaries = request.dbsession.query(Summary).filter_by(paper=paper, review_status=ENUM_Summary_review_status['public']).all()
     else:
         summaries = request.dbsession.query(Summary).filter_by(paper=paper, visibility=ENUM_Summary_visibility['public']).all()
     tips = request.dbsession.query(Tip).filter_by(paper=paper).all()
     num_tips = request.dbsession.query(Tip).filter_by(paper=paper).count()
 
     return dict(paper=paper,
-        summary_written=summary_written,
+        has_wrote=has_wrote,
         num_summaries=num_summaries,
         summaries=summaries,
         tips=tips,
@@ -42,7 +62,7 @@ def add_summary(request):
         body = request.params['body']
         summary = Summary(creator=request.user, paper=paper, data=body)
         request.dbsession.add(summary)
-        next_url = request.route_url('edit_summary', arxiv_id=paper.arxiv_id)
+        next_url = request.route_url('view_paper', arxiv_id=paper.arxiv_id)
         return HTTPFound(location=next_url)
     save_url = request.route_url('add_summary', arxiv_id=paper.arxiv_id)
     return dict(paper=paper, save_url=save_url)
@@ -54,7 +74,7 @@ def edit_summary(request):
     paper = summary.paper
     if 'form.submitted' in request.params:
         summary.data = request.params['body']
-        next_url = request.route_url('edit_summary', arxiv_id=paper.arxiv_id)
+        next_url = request.route_url('view_paper', arxiv_id=paper.arxiv_id)
         return HTTPFound(location=next_url)
     save_url = request.route_url('edit_summary', arxiv_id=paper.arxiv_id)
     return dict(paper=paper, data=summary.data, save_url=save_url)
@@ -79,7 +99,7 @@ def edit_tip(request):
     paper = tip.paper
     if 'form.submitted' in request.params:
         tip.data = request.params['body']
-        next_url = request.route_url('edit_tip', arxiv_id=paper.arxiv_id, tip_id=tip.id)
+        next_url = request.route_url('view_paper', arxiv_id=paper.arxiv_id)
         return HTTPFound(location=next_url)
     save_url = request.route_url('edit_tip', arxiv_id=paper.arxiv_id, tip_id=tip.id)
     return dict(paper=paper, data=tip.data, save_url=save_url)
