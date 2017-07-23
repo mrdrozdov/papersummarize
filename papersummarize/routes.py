8aeb7b9d04def928ce58a0ebf6ce518476b69f1a
Andrew Drozdov <andrew@mrdrozdov.com>
2017-07-23 09:59:36 -0400

    Anyone can add/edit Tips.

--- papersummarize/routes.py
+++ papersummarize/routes.py
@@ -7,7 +7,7 @@ from pyramid.security import (
     Everyone,
 )
 
-from .models import Page, Paper, Summary
+from .models import Page, Paper, Summary, Tip
 
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
@@ -17,11 +17,52 @@ def includeme(config):
     config.add_route('view_paper', '/x/{arxiv_id}', factory=paper_factory)
     config.add_route('add_summary', '/x/{arxiv_id}/add_summary', factory=new_summary_factory)
     config.add_route('edit_summary', '/x/{arxiv_id}/edit_summary', factory=summary_factory)
+    config.add_route('add_tip', '/x/{arxiv_id}/add_tip', factory=new_tip_factory)
+    config.add_route('edit_tip', '/x/{arxiv_id}/{tip_id}/edit', factory=tip_factory)
     config.add_route('view_page', '/{pagename}', factory=page_factory)
     config.add_route('add_page', '/add_page/{pagename}',
                      factory=new_page_factory)
     config.add_route('edit_page', '/{pagename}/edit_page',
                      factory=page_factory)
+
+def new_tip_factory(request):
+    arxiv_id = request.matchdict['arxiv_id']
+    paper = request.dbsession.query(Paper).filter_by(arxiv_id=arxiv_id).first()
+    if paper is None:
+        raise HTTPNotFound
+    return NewTip(paper)
+
+class NewTip(object):
+    def __init__(self, paper):
+        self.paper = paper
+
+    def __acl__(self):
+        return [
+            (Allow, 'role:editor', 'create'),
+            (Allow, 'role:basic', 'create'),
+        ]
+
+def tip_factory(request):
+    arxiv_id = request.matchdict['arxiv_id']
+    tip_id = request.matchdict['tip_id']
+    paper = request.dbsession.query(Paper).filter_by(arxiv_id=arxiv_id).first()
+    if paper is None:
+        raise HTTPNotFound
+    tip = request.dbsession.query(Tip).filter_by(creator=request.user, id=tip_id, paper=paper).first()
+    if tip is None:
+        raise HTTPNotFound
+    return TipResource(tip)
+
+class TipResource(object):
+    def __init__(self, tip):
+        self.tip = tip
+
+    def __acl__(self):
+        return [
+            (Allow, Everyone, 'view'),
+            (Allow, 'role:editor', 'edit'),
+            (Allow, str(self.tip.creator_id), 'edit'),
+        ]
 
 def new_summary_factory(request):
     arxiv_id = request.matchdict['arxiv_id']
--- papersummarize/views/papers.py
+++ papersummarize/views/papers.py
@@ -14,7 +14,7 @@ from ..shared.enums import ENUM_User_is_leader, ENUM_Summary_visibility, ENUM_Su
 def view_paper(request):
     paper = request.context.paper
     summary_written = request.dbsession.query(Summary).filter_by(creator=request.user, paper=paper).count() == 1
-    summaries_written = request.dbsession.query(Summary).filter_by(paper=paper).count()
+    num_summaries = request.dbsession.query(Summary).filter_by(paper=paper).count()
 
     if request.user is not None:
         # TODO: If the user has written a summary, always show it.
@@ -25,12 +25,14 @@ def view_paper(request):
     else:
         summaries = request.dbsession.query(Summary).filter_by(paper=paper, visibility=ENUM_Summary_visibility['public']).all()
     tips = request.dbsession.query(Tip).filter_by(paper=paper).all()
+    num_tips = request.dbsession.query(Tip).filter_by(paper=paper).count()
 
     return dict(paper=paper,
         summary_written=summary_written,
-        summaries_written=summaries_written,
+        num_summaries=num_summaries,
         summaries=summaries,
-        tips=tips)
+        tips=tips,
+        num_tips=num_tips)
 
 @view_config(route_name='add_summary', renderer='../templates/add_summary.jinja2',
              permission='create')
@@ -55,4 +57,29 @@ def edit_summary(request):
         next_url = request.route_url('edit_summary', arxiv_id=paper.arxiv_id)
         return HTTPFound(location=next_url)
     save_url = request.route_url('edit_summary', arxiv_id=paper.arxiv_id)
-    return dict(paper=paper, summarydata=summary.data, save_url=save_url)
+    return dict(paper=paper, data=summary.data, save_url=save_url)
+
+@view_config(route_name='add_tip', renderer='../templates/add_tip.jinja2',
+             permission='create')
+def add_tip(request):
+    paper = request.context.paper
+    if 'form.submitted' in request.params:
+        body = request.params['body']
+        tip = Tip(creator=request.user, paper=paper, data=body)
+        request.dbsession.add(tip)
+        next_url = request.route_url('view_paper', arxiv_id=paper.arxiv_id)
+        return HTTPFound(location=next_url)
+    save_url = request.route_url('add_tip', arxiv_id=paper.arxiv_id)
+    return dict(paper=paper, save_url=save_url)
+
+@view_config(route_name='edit_tip', renderer='../templates/edit_tip.jinja2',
+             permission='edit')
+def edit_tip(request):
+    tip = request.context.tip
+    paper = tip.paper
+    if 'form.submitted' in request.params:
+        tip.data = request.params['body']
+        next_url = request.route_url('edit_tip', arxiv_id=paper.arxiv_id, tip_id=tip.id)
+        return HTTPFound(location=next_url)
+    save_url = request.route_url('edit_tip', arxiv_id=paper.arxiv_id, tip_id=tip.id)
+    return dict(paper=paper, data=tip.data, save_url=save_url)
