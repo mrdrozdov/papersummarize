153718eaf4a1c4698ec7c93f9126289a0361a4fb
Andrew Drozdov <andrew@mrdrozdov.com>
2017-11-20 04:01:23 +0000

    Datadog integration.

--- papersummarize/__init__.py
+++ papersummarize/__init__.py
@@ -1,13 +1,17 @@
 from pyramid.config import Configurator
+from ddtrace.contrib.pyramid import trace_pyramid
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['datadog_trace_service'] = 'papersummarize-web'
     config = Configurator(settings=settings)
+    trace_pyramid(config)
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
     config.include('.security')
     config.scan()
     return config.make_wsgi_app()
+
